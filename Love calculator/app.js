45415b56de7849e5eb15994972a8970f27bc5c71
Sugat Athawale <154099988+sugatathawale@users.noreply.github.com>
2024-08-22 19:07:21 +0530

    Add files via upload

---
+++ Love calculator/app.js
@@ -0,0 +1,13 @@
+function clickMe(){
+    var result =Math.floor(Math.random()*100);
+    document.querySelector(".sanam").value=result +"%";
+    if(result>60){
+        window.open("https://www.oyorooms.com/")
+    }else{
+        alert("you are not eligible for oyo")
+    }
+
+}
+function changeIt(){
+    document.querySelector()
+}
